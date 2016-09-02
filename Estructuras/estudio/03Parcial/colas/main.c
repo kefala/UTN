08721c0299f01ea54ca43c47413b0df5d56c64c0
kefala <martin.g.lobo@gmail.com>
2016-09-01 23:24:45 -0300

    Start to study

---
+++ Estructuras/estudio/03Parcial/colas/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct elemento {
+	int dato;
+	struct elemento *sig;
+} nodo;
+
+
+void cargar(nodo** , int);
+void mostrar(nodo*);
+void borrar(nodo** );
+nodo *nuevonodo(int);
+
+int main () {
+	nodo * lista;
+	int cant, i, n;
+
+	lista = NULL;
+	printf("\n Ingrese Dato: ");
+	scanf("%d", &n);
+	cargar(&lista,n);
+	
+	
+
+	printf("\n Llega acá");
+	mostrar(lista);
+
+	return 0;
+}
+
+void cargar(nodo** l, int x)
+{
+	nodo* nuevo;
+
+	if(*l == NULL)
+	{
+		nuevo = nuevonodo(x);
+		*l=nuevo;
+	}
+	else
+		cargar(&(*l)->sig,x);
+
+}
+
+void mostrar(nodo* l)
+{
+	if(l) {
+		printf("%3d",l->dato);
+		mostrar(l->sig);
+	}
+}
+
+void borrar(nodo** l)
+{
+	nodo *aux;
+	aux=*l;
+	if(*l)
+	{
+		printf("\nNodo a eliminar: %d",aux->dato);
+
+		*l = aux->sig;
+		free(aux);
+
+		borrar(l);
+	}
+}
+
+nodo *nuevonodo(int x)
+{
+	nodo *a;
+	a=(nodo*)malloc(sizeof(nodo));
+
+	a->dato = x;
+	a->sig = NULL;
+	return (a);
+}
